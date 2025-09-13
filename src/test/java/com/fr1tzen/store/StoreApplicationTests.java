ffa684b774f677c383c0b645fdae86af30f496b1
ericfr1tzenvalle <ericfritzenvalle@gmail.com>
2025-09-12 23:26:44 -0300

    feat(skeleton): implementar esqueleto do projeto.

---
+++ src/test/java/com/fr1tzen/store/StoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fr1tzen.store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
